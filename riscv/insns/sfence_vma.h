37b9faec7d7916fb5575db2db133f8462e7c14c2
Udit Khanna <udit.khanna@sifive.com>
2019-12-05 14:50:30 -0800

    SFENCE.VMA requires S-mode

--- riscv/insns/sfence_vma.h
+++ riscv/insns/sfence_vma.h
@@ -1,2 +1,5 @@
+require_extension('S');
+bool vm_enabled = get_field(STATE.satp, (xlen == 32 ? SATP32_MODE : SATP64_MODE)) != SATP_MODE_OFF;
+require(vm_enabled);
 require_privilege(get_field(STATE.mstatus, MSTATUS_TVM) ? PRV_M : PRV_S);
 MMU.flush_tlb();
